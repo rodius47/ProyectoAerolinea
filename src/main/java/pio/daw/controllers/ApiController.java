1f31b145b726b2a94e67fd7366aea576cffc6867
unknown <rodrigo.mogar07@gmail.com>
2026-05-20 21:01:43 +0200

    controlador api tocado

--- src/main/java/pio/daw/controllers/ApiController.java
+++ src/main/java/pio/daw/controllers/ApiController.java
@@ -1,5 +1,36 @@
 package pio.daw.controllers;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import pio.daw.services.ServicioGeneral;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import pio.daw.models.Aerolinea;
+
+
+@RestController
 public class ApiController {
     // hay que crear en docker un contenedor que se llame proyectoSpringAerolinea
+
+    private final ServicioGeneral servicioGeneral;
+
+    public ApiController(ServicioGeneral servicioGeneral){
+        this.servicioGeneral = servicioGeneral;
+    }
+
+    @GetMapping
+    public List<Aerolinea> listarTodos() {
+        return servicioGeneral.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Aerolinea buscarPorId(@PathVariable Long id) {
+        return servicioGeneral.buscarPorId(id);
+    }
+    
 }
--- src/main/java/pio/daw/controllers/WebController.java
+++ src/main/java/pio/daw/controllers/WebController.java
@@ -1,5 +1,8 @@
 package pio.daw.controllers;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class WebController {
     
 }
