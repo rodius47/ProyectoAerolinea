2bc0b0ed15b011439ba38af5942e3876616d08d8
rodius47 <rodrigo.mogar07@gmail.com>
2026-05-21 11:02:35 +0200

    con docker

--- src/main/java/pio/daw/controllers/ApiController.java
+++ src/main/java/pio/daw/controllers/ApiController.java
@@ -1,16 +1,8 @@
 package pio.daw.controllers;
 
-import java.util.List;
-
 import org.springframework.web.bind.annotation.RestController;
 
 import pio.daw.services.ServicioGeneral;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import pio.daw.models.Aerolinea;
 
 
 @RestController
@@ -23,14 +15,14 @@ public class ApiController {
         this.servicioGeneral = servicioGeneral;
     }
 
-    @GetMapping
-    public List<Aerolinea> listarTodos() {
-        return servicioGeneral.listarTodos();
-    }
+    // @GetMapping
+    // public List<Aerolinea> listarTodos() {
+    //     return servicioGeneral.listarTodos();
+    // }
 
-    @GetMapping("/{id}")
-    public Aerolinea buscarPorId(@PathVariable Long id) {
-        return servicioGeneral.buscarPorId(id);
-    }
+    // @GetMapping("/{id}")
+    // public Aerolinea buscarPorId(@PathVariable Long id) {
+    //     return servicioGeneral.buscarPorId(id);
+    // }
     
 }
